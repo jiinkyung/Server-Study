3e1e403401353b4daaee299c8f43e931667a3d56
jiinkyung <jinkyung010310@gmail.com>
2023-11-07 16:44:13 +0900

    에러 핸들러 생성

--- src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -19,7 +19,11 @@ public enum ErrorStatus implements BaseErrorCode {
 
 
     // 멤버 관련 응답
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
+    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
 
+    // Article Error
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
     // ~~~ 관련 응답 ....
 
 
